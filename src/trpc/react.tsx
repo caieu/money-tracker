3d4089978b22acae66d2efd926ab6db4ca69fdff
Caainã Jeronimo <caainaje@gmail.com>
2024-09-21 16:05:57 -0300

    fix imports

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -3,12 +3,12 @@
 import { QueryClientProvider, type QueryClient } from "@tanstack/react-query";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
-import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import { useState } from "react";
 import SuperJSON from "superjson";
 
-import { type AppRouter } from "~/server/api/root";
+import { type AppRouter } from "@/server/api/root";
 import { createQueryClient } from "./query-client";
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 
 let clientQueryClientSingleton: QueryClient | undefined = undefined;
 const getQueryClient = () => {
@@ -57,7 +57,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
           },
         }),
       ],
-    })
+    }),
   );
 
   return (
